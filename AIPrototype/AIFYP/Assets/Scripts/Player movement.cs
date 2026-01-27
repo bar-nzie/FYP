b42261ff749f77f20f82172eb3f3ab1700777b71
bar-nzie <146812185+bar-nzie@users.noreply.github.com>
2026-01-27 13:50:53 +0000

    FYP progress save

--- AIPrototype/AIFYP/Assets/Scripts/Player movement.cs
+++ AIPrototype/AIFYP/Assets/Scripts/Player movement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Playermovement : MonoBehaviour
 {
@@ -11,9 +12,28 @@ public class Playermovement : MonoBehaviour
     public float mouseSensitivity = 100f;
     public Transform cameraHolder;
 
+    [Header]
+
     private CharacterController controller;
     private Vector3 velocity;
     private float xRotation = 0f;
+
+    private InputSystem_Actions inputActions;
+
+    private void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,10 +51,9 @@ public class Playermovement : MonoBehaviour
 
     void Move()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        Vector2 input = inputActions.Player.Move.ReadValue<Vector2>();
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = transform.right * input.x + transform.forward * input.y;
         controller.Move(move * speed * Time.deltaTime);
 
         if (controller.isGrounded && velocity.y < 0)
@@ -42,11 +61,26 @@ public class Playermovement : MonoBehaviour
             velocity.y = -2f;
         }
 
-        if(Input.GetButtonDown("Jump") && controller.isGrounded)
+        if (inputActions.Player.Jump.WasPressedThisFrame() && controller.isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
-        
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void MouseLook()
+    {
+        Vector2 mouseDelta = inputActions.Player.Look.ReadValue<Vector2>();
+
+        float mouseX = mouseDelta.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = mouseDelta.y * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cameraHolder.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
     }
 }
